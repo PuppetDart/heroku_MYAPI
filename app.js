dda26668756dee6641c9096cefef37554189e96b
Abhishek Pandey <chiseldart@hotmail.com>
2022-10-23 03:12:25 +0530

    trying to add CORS support

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 const express = require("express");
 
 const app = express();
+app.use((req,res,next)=>{
+  res.setHeader('Access-Control-Allow-Origin','*');
+  res.setHeader('Access-Control-Allow-Methods','GET,POST,PUT,PATCH,DELETE');
+  res.setHeader('Access-Control-Allow-Methods','Content-Type','Authorization');
+  next(); 
+});
 app.get("/", (req, res) => {
 
   var objArr = [

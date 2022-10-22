db799a52f0aeb09e5dfea0b9a57605778f0909c1
Abhishek Pandey <chiseldart@hotmail.com>
2022-10-23 02:36:02 +0530

    API modified for ContactAPP

--- app.js
+++ app.js
@@ -1,161 +1,161 @@
-const express=require("express");
+const express = require("express");
 
-const app=express();
-app.get("/",(req,res)=>{
+const app = express();
+app.get("/", (req, res) => {
 
-var objArry =[
-  {
-    "id": 1,
-    "name": "Monica Rambeau",
-    "number": 2226364245,
-    "occupation": "SWORD Agent"
-  },
-  {
-    "id": 2,
-    "name": "Thaddeus Ross",
-    "number": 2223073728,
-    "occupation": "Lieutenant General of the United States Army"
-  },
-  {
-    "id": 3,
-    "name": "Peter Jason Quill",
-    "number": 7926587328,
-    "occupation": "Star Lord"
-  },
-  {
-    "id": 4,
-    "name": "Samuel Thomas Wilson",
-    "number": 6345746870,
-    "occupation": "Avenger"
-  },
-  {
-    "id": 5,
-    "name": "Yondu Udonta",
-    "number": 2266336920,
-    "occupation": "Ravager"
-  },
-  {
-    "id": 6,
-    "name": "Carol Susan Jane Danvers",
-    "number": 2227667306,
-    "occupation": "Captain Marvel"
-  },
-  {
-    "id": 7,
-    "name": "Robert Downey Jr",
-    "number": 3000300030,
-    "occupation": "Actor"
-  },
-  {
-    "id": 8,
-    "name": "Henry Jonathan Hank Pym",
-    "number": 2228615444,
-    "occupation": "Scientist"
-  },
-  {
-    "id": 9,
-    "name": "Anthony Edward Tony Stark",
-    "number": 2223463307,
-    "occupation": "Weapons Manufacturer"
-  },
-  {
-    "id": 10,
-    "name": "Robert Bruce Banner",
-    "number": 2269582324,
-    "occupation": "Scientist"
-  },
-  {
-    "id": 11,
-    "name": "Thor Odinson",
-    "number": 4027844202,
-    "occupation": "God"
-  },
-  {
-    "id": 12,
-    "name": "Logan",
-    "number": 2228682405,
-    "occupation": "Car Driver"
-  },
-  {
-    "id": 13,
-    "name": "Clinton Francis Barton",
-    "number": 2223400233,
-    "occupation": "Archer"
-  },
-  {
-    "id": 14,
-    "name": "Pietro Maximoff",
-    "number": 2224031155,
-    "occupation": "Speedster"
-  },
-  {
-    "id": 15,
-    "name": "Wanda Maximoff",
-    "number": 4428255832,
-    "occupation": "Sorceror"
-  },
-  {
-    "id": 16,
-    "name": "Eric Brooks Blade",
-    "number": 2223883446,
-    "occupation": "Vampire"
-  },
-  {
-    "id": 17,
-    "name": "T Challa",
-    "number": 2228786585,
-    "occupation": "King"
-  },
-  {
-    "id": 18,
-    "name": "Victor Shade",
-    "number": 1125279946,
-    "occupation": "Vision"
-  },
-  {
-    "id": 19,
-    "name": "Dane Whitman",
-    "number": 1165807892,
-    "occupation": "Black Knight"
-  },
-  {
-    "id": 20,
-    "name": "Natalia Alianovna Romanova",
-    "number": 2228463880,
-    "occupation": "Shield Agent"
-  },
-  {
-    "id": 21,
-    "name": "Henry P McCoy",
-    "number": 2224071511,
-    "occupation": "Doctor"
-  },
-  {
-    "id": 22,
-    "name": "Thanos",
-    "number": 1245634576,
-    "occupation": "Villain"
-  },
-  {
-    "id": 23,
-    "name": "Doctor Stephen Vincent Strange",
-    "number": 5900000095,
-    "occupation": "Sorceror Supreme"
-  },
-  {
-    "id": 24,
-    "name": "Benedict Wong",
-    "number": 2222676497,
-    "occupation": "Sorceror"
-  },
-  {
-    "id": 25,
-    "name": "Wade Wilson",
-    "number": 8888313888,
-    "occupation": "Deadpool"
-  }
-];
-res.json(objArry);
+  var objArr = [
+    {
+      "id": 1,
+      "name": "Monica Rambeau",
+      "number": 2226364245,
+      "occupation": "SWORD Agent"
+    },
+    {
+      "id": 2,
+      "name": "Thaddeus Ross",
+      "number": 2223073728,
+      "occupation": "Lieutenant General of the United States Army"
+    },
+    {
+      "id": 3,
+      "name": "Peter Jason Quill",
+      "number": 7926587328,
+      "occupation": "Star Lord"
+    },
+    {
+      "id": 4,
+      "name": "Samuel Thomas Wilson",
+      "number": 6345746870,
+      "occupation": "Avenger"
+    },
+    {
+      "id": 5,
+      "name": "Yondu Udonta",
+      "number": 2266336920,
+      "occupation": "Ravager"
+    },
+    {
+      "id": 6,
+      "name": "Carol Susan Jane Danvers",
+      "number": 2227667306,
+      "occupation": "Captain Marvel"
+    },
+    {
+      "id": 7,
+      "name": "Robert Downey Jr",
+      "number": 3000300030,
+      "occupation": "Actor"
+    },
+    {
+      "id": 8,
+      "name": "Henry Jonathan Hank Pym",
+      "number": 2228615444,
+      "occupation": "Scientist"
+    },
+    {
+      "id": 9,
+      "name": "Anthony Edward Tony Stark",
+      "number": 2223463307,
+      "occupation": "Weapons Manufacturer"
+    },
+    {
+      "id": 10,
+      "name": "Robert Bruce Banner",
+      "number": 2269582324,
+      "occupation": "Scientist"
+    },
+    {
+      "id": 11,
+      "name": "Thor Odinson",
+      "number": 4027844202,
+      "occupation": "God"
+    },
+    {
+      "id": 12,
+      "name": "Logan",
+      "number": 2228682405,
+      "occupation": "Car Driver"
+    },
+    {
+      "id": 13,
+      "name": "Clinton Francis Barton",
+      "number": 2223400233,
+      "occupation": "Archer"
+    },
+    {
+      "id": 14,
+      "name": "Pietro Maximoff",
+      "number": 2224031155,
+      "occupation": "Speedster"
+    },
+    {
+      "id": 15,
+      "name": "Wanda Maximoff",
+      "number": 4428255832,
+      "occupation": "Sorceror"
+    },
+    {
+      "id": 16,
+      "name": "Eric Brooks Blade",
+      "number": 2223883446,
+      "occupation": "Vampire"
+    },
+    {
+      "id": 17,
+      "name": "T Challa",
+      "number": 2228786585,
+      "occupation": "King"
+    },
+    {
+      "id": 18,
+      "name": "Victor Shade",
+      "number": 1125279946,
+      "occupation": "Vision"
+    },
+    {
+      "id": 19,
+      "name": "Dane Whitman",
+      "number": 1165807892,
+      "occupation": "Black Knight"
+    },
+    {
+      "id": 20,
+      "name": "Natalia Alianovna Romanova",
+      "number": 2228463880,
+      "occupation": "Shield Agent"
+    },
+    {
+      "id": 21,
+      "name": "Henry P McCoy",
+      "number": 2224071511,
+      "occupation": "Doctor"
+    },
+    {
+      "id": 22,
+      "name": "Thanos",
+      "number": 1245634576,
+      "occupation": "Villain"
+    },
+    {
+      "id": 23,
+      "name": "Doctor Stephen Vincent Strange",
+      "number": 5900000095,
+      "occupation": "Sorceror Supreme"
+    },
+    {
+      "id": 24,
+      "name": "Benedict Wong",
+      "number": 2222676497,
+      "occupation": "Sorceror"
+    },
+    {
+      "id": 25,
+      "name": "Wade Wilson",
+      "number": 8888313888,
+      "occupation": "Deadpool"
+    }
+  ];
+  res.json(objArr);
 })
 
 app.listen(process.env.PORT || 3000);
